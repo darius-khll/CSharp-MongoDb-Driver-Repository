ecfbc5565048fb5b5329f14e226ecc34543964bf
Ali Khalili <ali.khll.1994@gmail.com>
2016-11-26 17:10:30 +0330

    post model added

---
+++ AspCoreMongo/src/AspCoreMongo/Model/Post.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace AspCoreMongo.Model
+{
+    public class Post
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.UtcNow;
+        public int UserId { get; set; } = 0;
+    }
+}
