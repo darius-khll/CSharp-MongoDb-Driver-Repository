8456546d1473b8145c3a2c7cfb538340d2b21e6b
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-01 20:54:22 +0330

    mongo context added

---
+++ AspCoreMongo/src/AspCoreMongo/Context/MongoContext.cs
@@ -0,0 +1,30 @@
+﻿using AspCoreMongo.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMongo.Context
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Post> Posts
+        {
+            get
+            {
+                return _database.GetCollection<Post>("Post");
+            }
+        }
+    }
+}
