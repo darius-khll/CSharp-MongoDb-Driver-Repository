c11fb689c7e739aadc3784ae85b56b69b58251ca
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-01 20:55:25 +0330

    settings handler injected

---
+++ AspCoreMongo/src/AspCoreMongo/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMongo
+{
+    public class Settings
+    {
+        public string ConnectionString;
+        public string Database;
+    }
+}
