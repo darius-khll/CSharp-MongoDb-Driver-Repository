7d5f4242e8c7da1a9a6ced282bed80d6604d39c5
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-01 20:54:09 +0330

    IPostRepository added

---
+++ AspCoreMongo/src/AspCoreMongo/Context/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using AspCoreMongo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMongo.Context
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+        Task<Post> GetPost(string id);
+        void AddPost(Post item);
+        Task<bool> RemovePost(string id);
+        void UpdatePost(string id, string body);
+
+        // it should be cautiously used (only in relation with development tests)
+        void RemoveAllPosts();
+    }
+}
